313622aa432a3480cc03e32e5a428246b756f72e
Achmadfadjry <achmadfadjryadlilfaqih@gmail.com>
2025-04-29 08:07:00 +0700

    menambahkan API Movies

---
+++ Controllers/MovieControllers.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using modul10_103022300159;
+
+namespace modul10_103022300159.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MovieControllers : ControllerBase
+    {
+        private static List<Movie> movieList = new List<Movie>
+        { 
+        new Movie("The Shawshank Redemption", "Frank Darabant", ["Tim Robbins", "Morgan freeman", "bob gunton", "william sadler"], "Two imprisoned men band over a number of years,finding solace and eventual redemption through act of common decency"),
+        new Movie("The god father", "Francis ford copolla", ["marlan brando", "al pacino", "james caan", "diane kaetan"], "the aging partiach of an organized crime dynasty in postwar new york city transfer control of his clandesitine empire to recultant youngest son"),
+        new Movie("the dark night", "christopher nolan", ["cristian bale", "heath ledger", "aoron eckhart", "michael coine"], "gandalf and arogorn lead the worold of men againnst of men suran army to draw his gaze from frado and som as they approach mount doom with the one ring"),
+
+        };
+[HttpGet]
+public ActionResult<IEnumerable<Movie>> GetAll()
+{
+    return Ok(movieList);
+}
+
+[HttpGet("{index}")]
+public ActionResult<Movie> GetByIndex(int index)
+{
+    if (index < 0 || index >= movieList.Count)
+        return NotFound("Index tidak ditemukan.");
+
+    return Ok(movieList[index]);
+}
+
+[HttpPost]
+public ActionResult AddMovie([FromBody] Movie newMovie)
+{
+    movieList.Add(newMovie);
+    return Ok("Movie berhasil ditambahkan.");
+}
+
+[HttpDelete("{index}")]
+public ActionResult DeleteMovie(int index)
+{
+    if (index < 0 || index >= movieList.Count)
+        return NotFound("Index tidak ditemukan.");
+
+    movieList.RemoveAt(index);
+    return Ok("Movie berhasil dihapus.");
+}
+    }
+}
+
+
---
+++ Movie.cs
@@ -0,0 +1,19 @@
+﻿namespace modul10_103022300159
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public List<string> stars { get; set; }
+        public string Description { get; set; }
+
+        public Movie (string title, string director,List <string> stars, string description)
+        {
+            this.Title = title;
+            this.Director = director;
+            this.stars = stars;
+            this.Description = description;
+            
+        }
+    }
+}
